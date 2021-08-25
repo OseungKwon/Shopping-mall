ab88f4c05352a0c09afc9a2b576e0ea34d4abc63
OseungKwon <00osos@naver.com>
2021-08-26 04:23:05 +0900

    Update ProductPage.js

--- client/src/pages/ProductPage.js
+++ client/src/pages/ProductPage.js
@@ -43,31 +43,38 @@ const ProductPage = (props) => {
   const productId = props.match.params.productId;
   const [Product, setProduct] = useState([]);
 
+  //
   useEffect(() => {
+    // 제품 아이디를 키로 가지고 있는 localStorage가 존재하지 않으면,
     if (window.localStorage.getItem(productId) === null) {
-      console.log("axios");
       console.log(productId);
+      // Axios를 통해 서버에서 제품 데이터를 받아온다.
       Axios.get(`/api/product/products_by_id?id=${productId}&type=single`).then(
         (response) => {
+          // 받아온 데이터를 useState(Product,setProduct)에 저장
           setProduct(response.data[0]);
         }
       );
+      // 제품 아이디를 키로 가지고 있는 localStorage가 존재하면,
     } else {
-      var k = JSON.parse(window.localStorage.getItem(productId));
-      setProduct(k);
+      // localStorage에서 데이터를 받아와 seProduct해준다.
+      const getData = JSON.parse(window.localStorage.getItem(productId));
+      setProduct(getData);
     }
   }, [productId]);
 
+  // useState는 비동기적으로 작동해 useEffect를 사용해야 한다.
   useEffect(() => {
+    // 페이지에 접속했을 때, Product는 []과 같이 초기화된 상태이므로 예외 처리
     if (Product.length !== 0) {
+      // 제품 아이디를 키로 가지고 있는 localStorage가 존재하지 않으면,
       if (window.localStorage.getItem(productId) === null) {
+        // Product 데이터를 localStorage에 제품아이디를 키로 하여 저장시킨다.
         const data = JSON.stringify(Product);
-        //console.log("a", data);
         window.localStorage.setItem(productId, data);
-        console.log("server");
+        console.log("server"); // 서버에서 데이터 받아옴
       } else {
-        var k = JSON.parse(window.localStorage.getItem("key"));
-        console.log("local", k);
+        console.log("local"); // 로컬에 저장된 데이터 사용
         console.log(productId, Product);
       }
     }
